2c51689e8beaf60085e057013c946ee07dbc8f99
hongki09 <73209580+hongki09@users.noreply.github.com>
2020-10-21 10:30:28 +0700

    Add files via upload
    first commit

---
+++ IF10K-10119915-Latihan7-Integer/src/if10k/pkg10119915/latihan7/integer/IF10K10119915Latihan7Integer.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119915.latihan7.integer;
+
+/**
+ *
+ * @author 
+ * Nama      : M Hongki Alfikram
+ * NIM       : 10119915
+ * Kelas     : IF-10K
+ * Deskripsi : Deklarasi Variable Integer.
+ */
+public class IF10K10119915Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        byte b;
+        short s;
+        int i;
+        long l;
+        
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        
+        System.out.println("byte :" + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+        
+    }
+    
+}
